8fa857063c7443a43c4204b34620a562b16d1b01
greatAdams01 <kingifean@gmail.com>
2022-07-22 05:04:17 +0100

    Fuzz testing

--- test/StakeContract.t.sol
+++ test/StakeContract.t.sol
@@ -9,16 +9,17 @@ import "../src/mocks/MockERC20.sol";
 contract StakeContractTest is Test {
     StakeContract public stakeContract;
     MockERC20 public token;
-    uint256 public constant AMOUNT = 1e18;
+    // uint256 public constant AMOUNT = 1e18;
 
     function setUp() public {
         stakeContract = new StakeContract();
         token = new MockERC20();
     }
 
-    function testStakingContract() public {
-        token.approve(address(stakeContract), AMOUNT);
-        bool success = stakeContract.stake(AMOUNT, address(token));
+    // fuzz testing
+    function testStakingContract(uint16 amount) public {
+        token.approve(address(stakeContract), amount);
+        bool success = stakeContract.stake(amount, address(token));
         assertTrue(success);
     }
 }
