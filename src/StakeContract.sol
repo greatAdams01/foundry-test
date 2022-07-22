2ee56fc37c6e41b924b77f43a39327fd078486ab
greatAdams01 <kingifean@gmail.com>
2022-07-22 01:09:13 +0100

    staking code

---
+++ src/StakeContract.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+
+error StakeContract_TransferFailed();
+
+contract StakeContract {
+  mapping(address => uint256) public s_balances;
+
+  function stake(uint256 amount, address token) external returns (bool) {
+    s_balances[msg.sender] = s_balances[msg.sender] + amount;
+
+    bool success = IERC20(token).transferFrom(msg.sender, address(this), amount);
+    if(!success) revert StakeContract_TransferFailed();
+
+    return success;
+  }
+
+}
